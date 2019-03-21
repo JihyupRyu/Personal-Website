0363933952a3ba9d66b54504aeef6b0a2dd74829
hopeinvader <ryujihyup@gmail.com>
2019-03-21 14:07:10 +0800

    Last change

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,30 +1,20 @@
 $(document).ready(function () {
+
+    // Smooth scroll initalisation code, from slack goes here. 
+
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
     $('#dismiss, .overlay').on('click', function () {
-        // hide sidebar
         $('#sidebar').removeClass('active');
-        // hide overlay
         $('.overlay').removeClass('active');
     });
 
     $('#sidebarCollapse').on('click', function () {
-        // open sidebar
         $('#sidebar').addClass('active');
-        // fade in the overlay
         $('.overlay').addClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
-
-$(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
